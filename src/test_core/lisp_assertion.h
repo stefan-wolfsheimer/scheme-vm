845850548a45d6616ac282bfa6b29da7695e22fd
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2016-08-16 20:03:12 +0200

    added assertions of lisp return types

--- src/test_core/lisp_assertion.h
+++ src/test_core/lisp_assertion.h
@@ -25,58 +25,69 @@ assertion_t * assertion_create_lisp_code(const char * file,
                                                       0)))              \
   { return; }
 
-#define CREATE_CHECK_LISP_CODE_EQ(__TEST__, __EXPR__, __CODE__)        \
+#define CREATE_CHECK_LISP_CODE_EQ(__TEST__, __EXPR__, __CODE__)         \
   unit_add_assertion((__TEST__),                                        \
                      assertion_create_lisp_code(__FILE__,               \
                                                 __LINE__,               \
                                                 (#__EXPR__),            \
                                                 (__EXPR__),             \
-                                                CODE,                   \
+                                                (__CODE__),             \
                                                 "==",                   \
                                                 1))
 
 
-#define ASSERT_IS_OK(__TEST__, __EXPR__)                                \
-  CREATE_ASSERT_LISP_CODE_EQ((__TEST__), (__EXPR__), LISP_OK)
+#define ASSERT_IS_OK(__TST__, __EXPR__)                         \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_OK)
 
-#define ASSERT_IS_UNSUPPORTED(__TST__, __EXPR__)         \
-  ASSERT_EQ_I((__TST__), (__EXPR__), LISP_UNSUPPORTED)
+#define ASSERT_IS_ALLOC_ERROR(__TST__, __EXPR__)                        \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_ALLOC_ERROR)
 
-#define ASSERT_IS_COMPILATION_ERROR(__TST__, __EXPR__)          \
-  ASSERT_EQ_I((__TST__), (__EXPR__), LISP_COMPILATION_ERROR)
+#define ASSERT_IS_TYPE_ERROR(__TST__, __EXPR__)                         \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_TYPE_ERROR)
 
-#define ASSERT_IS_UNDEFINED(__TST__, __EXPR__)          \
-  ASSERT_EQ_I((__TST__), (__EXPR__), LISP_UNDEFINED)
+#define ASSERT_IS_EVAL_ERROR(__TST__, __EXPR__)                         \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_EVAL_ERROR)
 
-#define ASSERT_IS_STACK_OVERFLOW(__TST__, __EXPR__)     \
-  ASSERT_EQ_I((__TST__), (__EXPR__), LISP_STACK_OVERFLOW)
+#define ASSERT_IS_UNSUPPORTED(__TST__, __EXPR__)                        \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_UNSUPPORTED)
 
-#define ASSERT_IS_ALLOC_ERROR(__TST__, __EXPR__)        \
-  ASSERT_EQ_I((__TST__), (__EXPR__), LISP_ALLOC_ERROR)
+#define ASSERT_IS_UNDEFINED(__TST__, __EXPR__)                          \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_UNDEFINED)
 
-#define CHECK_IS_OK(__TEST__, __EXPR__)                                 \
-  unit_add_assertion((__TEST__),                                        \
-                     assertion_create_lisp_code(__FILE__,               \
-                                                __LINE__,               \
-                                                (#__EXPR__),            \
-                                                (__EXPR__),             \
-                                                LISP_OK,                \
-                                                "==",                   \
-                                                1))
+#define ASSERT_IS_RANGE_ERROR(__TST__, __EXPR__)                        \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_RANGE_ERROR)
 
-#define CHECK_IS_UNSUPPORTED(__TST__, __EXPR__)         \
-  CHECK_EQ_I((__TST__), (__EXPR__), LISP_UNSUPPORTED)
+#define ASSERT_IS_COMPILATION_ERROR(__TST__, __EXPR__)                  \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_COMPILATION_ERROR)
+
+#define ASSERT_IS_STACK_OVERFLOW(__TST__, __EXPR__)                     \
+  CREATE_ASSERT_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_STACK_OVERFLOW)
+
+#define CHECK_IS_OK(__TST__, __EXPR__)                          \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_OK) 
+
+#define CHECK_IS_ALLOC_ERROR(__TST__, __EXPR__)                         \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_ALLOC_ERROR) 
+
+#define CHECK_IS_TYPE_ERROR(__TST__, __EXPR__)                          \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_TYPE_ERROR) 
+
+#define CHECK_IS_EVAL_ERROR(__TST__, __EXPR__)                          \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_EVAL_ERROR) 
+
+#define CHECK_IS_UNSUPPORTED(__TST__, __EXPR__)                         \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_UNSUPPORTED) 
+
+#define CHECK_IS_UNDEFINED(__TST__, __EXPR__)                           \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_UNDEFINED) 
+
+#define CHECK_IS_RANGE_ERROR(__TST__, __EXPR__)                         \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_RANGE_ERROR) 
 
 #define CHECK_IS_COMPILATION_ERROR(__TST__, __EXPR__)          \
-  CHECK_EQ_I((__TST__), (__EXPR__), LISP_COMPILATION_ERROR)
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_COMPILATION_ERROR) 
 
-#define CHECK_IS_UNDEFINED(__TST__, __EXPR__)          \
-  CHECK_EQ_I((__TST__), (__EXPR__), LISP_UNDEFINED)
-
-#define CHECK_IS_STACK_OVERFLOW(__TST__, __EXPR__)     \
-  CHECK_EQ_I((__TST__), (__EXPR__), LISP_STACK_OVERFLOW)
-
-#define CHECK_IS_ALLOC_ERROR(__TST__, __EXPR__)        \
-  CHECK_EQ_I((__TST__), (__EXPR__), LISP_ALLOC_ERROR)
+#define CHECK_IS_STACK_OVERFLOW(__TST__, __EXPR__)                      \
+  CREATE_CHECK_LISP_CODE_EQ((__TST__), (__EXPR__), LISP_STACK_OVERFLOW) 
 
 #endif
--- src/test_core/test_lisp_assertion.c
+++ src/test_core/test_lisp_assertion.c
@@ -10,20 +10,29 @@ static void test_check_return_codes(unit_test_t * tst)
   ASSERT(tst,       CHECK_IS_OK(tst2, LISP_OK));
   ASSERT_FALSE(tst, CHECK_IS_OK(tst2, LISP_UNSUPPORTED));
 
+  ASSERT(tst,       CHECK_IS_ALLOC_ERROR(tst2, LISP_ALLOC_ERROR));
+  ASSERT_FALSE(tst, CHECK_IS_ALLOC_ERROR(tst2, LISP_UNSUPPORTED));
+
+  ASSERT(tst,       CHECK_IS_TYPE_ERROR(tst2, LISP_TYPE_ERROR));
+  ASSERT_FALSE(tst, CHECK_IS_TYPE_ERROR(tst2, LISP_UNSUPPORTED));
+
+  ASSERT(tst,       CHECK_IS_EVAL_ERROR(tst2, LISP_EVAL_ERROR));
+  ASSERT_FALSE(tst, CHECK_IS_EVAL_ERROR(tst2, LISP_UNSUPPORTED));
+
   ASSERT(tst,       CHECK_IS_UNSUPPORTED(tst2, LISP_UNSUPPORTED));
   ASSERT_FALSE(tst, CHECK_IS_UNSUPPORTED(tst2, LISP_OK));
-
-  ASSERT(tst,       CHECK_IS_COMPILATION_ERROR(tst2, LISP_COMPILATION_ERROR));
-  ASSERT_FALSE(tst, CHECK_IS_COMPILATION_ERROR(tst2, LISP_UNSUPPORTED));
 
   ASSERT(tst,       CHECK_IS_UNDEFINED(tst2, LISP_UNDEFINED));
   ASSERT_FALSE(tst, CHECK_IS_UNDEFINED(tst2, LISP_UNSUPPORTED));
 
+  ASSERT(tst,       CHECK_IS_RANGE_ERROR(tst2, LISP_RANGE_ERROR));
+  ASSERT_FALSE(tst, CHECK_IS_RANGE_ERROR(tst2, LISP_UNSUPPORTED));
+
+  ASSERT(tst,       CHECK_IS_COMPILATION_ERROR(tst2, LISP_COMPILATION_ERROR));
+  ASSERT_FALSE(tst, CHECK_IS_COMPILATION_ERROR(tst2, LISP_UNSUPPORTED));
+
   ASSERT(tst,       CHECK_IS_STACK_OVERFLOW(tst2, LISP_STACK_OVERFLOW));
   ASSERT_FALSE(tst, CHECK_IS_STACK_OVERFLOW(tst2, LISP_UNSUPPORTED));
-
-  ASSERT(tst,       CHECK_IS_ALLOC_ERROR(tst2, LISP_ALLOC_ERROR));
-  ASSERT_FALSE(tst, CHECK_IS_ALLOC_ERROR(tst2, LISP_UNSUPPORTED));
 
   unit_free_context(ctx);
 }
@@ -31,11 +40,15 @@ static void test_check_return_codes(unit_test_t * tst)
 static void test_assert_return_codes(unit_test_t * tst) 
 {
   ASSERT_IS_OK(tst, LISP_OK);
+  ASSERT_IS_ALLOC_ERROR(tst, LISP_ALLOC_ERROR);
+  ASSERT_IS_TYPE_ERROR(tst, LISP_TYPE_ERROR);
+  ASSERT_IS_EVAL_ERROR(tst, LISP_EVAL_ERROR);
   ASSERT_IS_UNSUPPORTED(tst, LISP_UNSUPPORTED);
+  ASSERT_IS_UNDEFINED(tst, LISP_UNDEFINED);
+  ASSERT_IS_RANGE_ERROR(tst, LISP_RANGE_ERROR);
   ASSERT_IS_COMPILATION_ERROR(tst, LISP_COMPILATION_ERROR);
   ASSERT_IS_UNDEFINED(tst, LISP_UNDEFINED);
   ASSERT_IS_STACK_OVERFLOW(tst, LISP_STACK_OVERFLOW);
-  ASSERT_IS_ALLOC_ERROR(tst, LISP_ALLOC_ERROR);
 }
 
 static void test_assertion_is_ok_failure(unit_test_t * tst) 
@@ -43,14 +56,24 @@ static void test_assertion_is_ok_failure(unit_test_t * tst)
   ASSERT_IS_OK(tst, LISP_UNSUPPORTED);
 }
 
+static void test_assertion_is_alloc_error_failure(unit_test_t * tst) 
+{
+  ASSERT_IS_ALLOC_ERROR(tst, LISP_UNSUPPORTED);
+}
+
+static void test_assertion_is_type_error_failure(unit_test_t * tst) 
+{
+  ASSERT_IS_TYPE_ERROR(tst, LISP_UNSUPPORTED);
+}
+
+static void test_assertion_is_eval_error_failure(unit_test_t * tst) 
+{
+  ASSERT_IS_EVAL_ERROR(tst, LISP_UNSUPPORTED);
+}
+
 static void test_assertion_is_unsupported_failure(unit_test_t * tst) 
 {
   ASSERT_IS_UNSUPPORTED(tst, LISP_OK);
-}
-
-static void test_assertion_is_compilation_error_failure(unit_test_t * tst) 
-{
-  ASSERT_IS_COMPILATION_ERROR(tst, LISP_OK);
 }
 
 static void test_assertion_is_undefined_failure(unit_test_t * tst) 
@@ -58,27 +81,37 @@ static void test_assertion_is_undefined_failure(unit_test_t * tst)
   ASSERT_IS_UNDEFINED(tst, LISP_OK);
 }
 
+static void test_assertion_is_range_error_failure(unit_test_t * tst) 
+{
+  ASSERT_IS_RANGE_ERROR(tst, LISP_OK);
+}
+
+static void test_assertion_is_compilation_error_failure(unit_test_t * tst) 
+{
+  ASSERT_IS_COMPILATION_ERROR(tst, LISP_OK);
+}
+
 static void test_assertion_is_stack_overflow_failure(unit_test_t * tst) 
 {
   ASSERT_IS_STACK_OVERFLOW(tst, LISP_OK);
 }
 
-static void test_assertion_is_alloc_error_failure(unit_test_t * tst) 
-{
-  ASSERT_IS_ALLOC_ERROR(tst, LISP_OK);
-}
 
 static void test_assert_return_code_failures(unit_test_t * tst) 
 {
   unit_context_t * ctx = unit_create_context();
   unit_suite_t * suite = unit_create_suite(ctx, "suite1");
-  size_t n_tests = 6;
+  size_t n_tests = 9;
   TEST(suite, test_assertion_is_ok_failure);
-  TEST(suite, test_assertion_is_unsupported_failure);
-  TEST(suite, test_assertion_is_compilation_error_failure);
-  TEST(suite, test_assertion_is_undefined_failure);
-  TEST(suite, test_assertion_is_stack_overflow_failure);
   TEST(suite, test_assertion_is_alloc_error_failure);
+  TEST(suite, test_assertion_is_type_error_failure);
+  TEST(suite, test_assertion_is_eval_error_failure);
+  TEST(suite, test_assertion_is_unsupported_failure);
+  TEST(suite, test_assertion_is_undefined_failure);
+  TEST(suite, test_assertion_is_range_error_failure);
+  TEST(suite, test_assertion_is_compilation_error_failure);
+  TEST(suite, test_assertion_is_stack_overflow_failure);
+
   unit_run(stdout, ctx);
   ASSERT_EQ_U(tst, ctx->n_suites_passed, 0);
   ASSERT_EQ_U(tst, ctx->n_suites_failed, 1);
