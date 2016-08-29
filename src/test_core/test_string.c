f4cd5b3b5ec1da06284be06ac0ff058a3aed88c3
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2016-08-29 09:40:23 +0200

    using specific assertions in string unit tests, replacing tabs with spaces

--- src/test_core/test_string.c
+++ src/test_core/test_string.c
@@ -1,9 +1,8 @@
 #include "util/unit_test.h"
 #include "util/xmalloc.h"
 #include "core/lisp_vm.h" 
+#include "test_core/lisp_assertion.h"
 
-/**************************************************************/
-/* @todo create mock object type */
 static void test_create_string(unit_test_t * tst)
 {
   memcheck_begin();
@@ -14,12 +13,12 @@ static void test_create_string(unit_test_t * tst)
   ASSERT(tst,         LISP_IS_STRING(&str));
   ASSERT(tst,         LISP_REFCOUNT(&str) == 1);
   ASSERT_EQ_U(tst,    lisp_string_length(LISP_AS(&str,
-						 lisp_string_t)), 3u);
+                                                 lisp_string_t)), 3u);
   ASSERT_EQ_CSTR(tst, lisp_c_string(&str), "abc");
   ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "abc"),0);
   ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "def"),0);
   ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "012"),0);
-  ASSERT_FALSE(tst,   lisp_unset_object(vm, &str));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
   ASSERT(tst,         LISP_IS_NIL(&str));
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
@@ -37,8 +36,8 @@ static void test_sprintf(unit_test_t * tst)
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 1u);
   ASSERT_EQ_CSTR(tst, lisp_c_string(&str), "1 ff abc");
   ASSERT_EQ_U(tst,    lisp_string_length(LISP_AS(&str,
-						 lisp_string_t)), 8u);
-  ASSERT_FALSE(tst,   lisp_unset_object(vm, &str));
+                                                 lisp_string_t)), 8u);
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
   ASSERT(tst,         LISP_IS_NIL(&str));
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
@@ -51,12 +50,12 @@ static void test_lisp_string_cmp_c_string(unit_test_t * tst)
   lisp_vm_t * vm = lisp_create_vm(&lisp_vm_default_param);
   lisp_cell_t str;
   lisp_make_string(vm, &str, "abc");
-  ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "abc"),0);
-  ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "xxx"),0);
-  ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "abcdef"),0);
-  ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "000"),0);
-  ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "ab"),0);
-  lisp_unset_object(vm, &str);
+  ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "abc"), 0);
+  ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "xxx"), 0);
+  ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "abcdef"), 0);
+  ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "000"), 0);
+  ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&str, lisp_string_t), "ab"), 0);
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
   lisp_free_vm(vm);
   memcheck_end();
 }
@@ -69,14 +68,14 @@ static void test_copy_string(unit_test_t * tst)
   lisp_make_string(vm, &str, "abc");
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 1u);
   
-  lisp_copy_object(vm, &copy1, &str);
+  ASSERT_IS_OK(tst,   lisp_copy_object(vm, &copy1, &str));
   ASSERT(tst,         LISP_IS_OBJECT(&copy1));
   ASSERT(tst,         LISP_IS_STRING(&copy1));
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 2u);
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&copy1), 2u);
   ASSERT_EQ_PTR(tst,  lisp_c_string(&str), lisp_c_string(&copy1));
 
-  lisp_copy_object(vm, &copy2, &copy1);
+  ASSERT_IS_OK(tst,   lisp_copy_object(vm, &copy2, &copy1))
   ASSERT(tst,         LISP_IS_OBJECT(&copy2));
   ASSERT(tst,         LISP_IS_STRING(&copy2));
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 3u);
@@ -89,21 +88,21 @@ static void test_copy_string(unit_test_t * tst)
   ASSERT_EQ_CSTR(tst, lisp_c_string(&copy1), "abc");
   ASSERT_EQ_CSTR(tst, lisp_c_string(&copy2), "abc");  
 
-  ASSERT_FALSE(tst, lisp_unset_object(vm, &str));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
   ASSERT(tst,         LISP_IS_NIL(&str));
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&copy1), 2u);
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&copy2), 2u);
   ASSERT_EQ_PTR(tst,  lisp_c_string(&copy2), lisp_c_string(&copy1));
   ASSERT_EQ_CSTR(tst, lisp_c_string(&copy1), "abc");
 
-  ASSERT_FALSE(tst, lisp_unset_object(vm, &copy2));
-  ASSERT(tst, LISP_REFCOUNT(&copy1) == 1);
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &copy2));
+  ASSERT(tst,         LISP_REFCOUNT(&copy1) == 1);
 
-  ASSERT_FALSE(tst, lisp_copy_object(vm, &copy2, &copy1));
+  ASSERT_IS_OK(tst,   lisp_copy_object(vm, &copy2, &copy1));
 
-  ASSERT(tst, LISP_REFCOUNT(&copy1) == 2);
-  ASSERT_FALSE(tst, lisp_unset_object(vm, &copy1));
-  ASSERT_FALSE(tst, lisp_unset_object(vm, &copy2));
+  ASSERT(tst,         LISP_REFCOUNT(&copy1) == 2);
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &copy1));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &copy2));
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
   memcheck_end();
@@ -114,19 +113,18 @@ static void test_make_substring_full(unit_test_t * tst)
   memcheck_begin();
   lisp_vm_t * vm = lisp_create_vm(&lisp_vm_default_param);
   lisp_cell_t str, substr_full;
-  lisp_make_string(vm, &str, "0123456789");
+  ASSERT_IS_OK(tst,   lisp_make_string(vm, &str, "0123456789"));
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 1u);
   ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr_full,
-					  LISP_AS(&str, lisp_string_t),
-					  0,
-					  10), LISP_OK);
+                                          &substr_full,
+                                          LISP_AS(&str, lisp_string_t),
+                                          0,
+                                          10), LISP_OK);
   ASSERT(tst,         LISP_IS_OBJECT(&substr_full));
   ASSERT(tst,         LISP_IS_STRING(&substr_full));
   ASSERT_EQ_PTR(tst,  lisp_c_string(&str), lisp_c_string(&substr_full));
   ASSERT_EQ_U(tst,    lisp_string_length(LISP_AS(&substr_full,
-						 lisp_string_t)), 10u);
-
+                                                 lisp_string_t)), 10u);
   ASSERT(tst,         LISP_REFCOUNT(&str) == 1);
 
   lisp_unset_object(vm, &str);
@@ -146,32 +144,31 @@ static void test_make_substring_empty(unit_test_t * tst)
   lisp_cell_t substr_empty1;
   lisp_cell_t substr_empty2;
   lisp_make_string(vm, &str, "0123456789");
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr_empty1,
-					  LISP_AS(&str, lisp_string_t),
-					  10,
-					  10), LISP_OK);
+  ASSERT_IS_OK(tst,    lisp_make_substring(vm, 
+                                           &substr_empty1,
+                                           LISP_AS(&str, lisp_string_t),
+                                           10,
+                                           10));
   ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr_empty1, 
-						       lisp_string_t), ""),0);
+                                                       lisp_string_t), ""),0);
   ASSERT_EQ_U(tst,    lisp_string_length(LISP_AS(&substr_empty1,
-						 lisp_string_t)), 0u);
+                                                 lisp_string_t)), 0u);
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 1u);
 
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr_empty2,
-					  LISP_AS(&str, lisp_string_t),
-					  0,
-					  0), LISP_OK);
+  ASSERT_IS_OK(tst,    lisp_make_substring(vm, 
+                                           &substr_empty2,
+                                           LISP_AS(&str, lisp_string_t),
+                                           0,
+                                           0));
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 1u);
   ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr_empty2, 
-						       lisp_string_t), ""),0);
+                                                       lisp_string_t), ""), 0);
   ASSERT_EQ_U(tst,    lisp_string_length(LISP_AS(&substr_empty2,
-						 lisp_string_t)), 0u);
+                                                 lisp_string_t)), 0u);
 
-  lisp_unset_object(vm, &str);
-  lisp_unset_object(vm, &substr_empty1);
-  lisp_unset_object(vm, &substr_empty2);
-
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &substr_empty1));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &substr_empty2));
 
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
@@ -183,34 +180,34 @@ static void test_make_substring_begin(unit_test_t * tst)
   memcheck_begin();
   lisp_vm_t * vm = lisp_create_vm(&lisp_vm_default_param);
   lisp_cell_t str, substr;
-  lisp_make_string(vm, &str, "0123456789");
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr,
-					  LISP_AS(&str, lisp_string_t),
-					  0,
-					  4), LISP_OK);
+  ASSERT_IS_OK(tst,    lisp_make_string(vm, &str, "0123456789"));
+  ASSERT_IS_OK(tst,    lisp_make_substring(vm, 
+                                           &substr,
+                                           LISP_AS(&str, lisp_string_t),
+                                           0,
+                                           4));
   ASSERT(tst,         LISP_IS_OBJECT(&substr));
   ASSERT(tst,         LISP_IS_STRING(&substr));
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&str), 1u);
   ASSERT_EQ_U(tst,    LISP_REFCOUNT(&substr), 1u);
   ASSERT_EQ_PTR(tst,  lisp_c_string(&str), lisp_c_string(&substr));
   ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t), 
-					       "0123"), 0);
+                                                       lisp_string_t), 
+                                               "0123"), 0);
   ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "01234"),0);
+                                                       lisp_string_t),
+                                               "01234"),0);
   ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "1234"), 0);
+                                                       lisp_string_t),
+                                               "1234"), 0);
   ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "0000"), 0);
+                                                       lisp_string_t),
+                                               "0000"), 0);
   ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "012"), 0);
-  lisp_unset_object(vm, &str);
-  lisp_unset_object(vm, &substr);
+                                                       lisp_string_t),
+                                               "012"), 0);
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &substr));
 
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
@@ -223,30 +220,35 @@ static void test_make_substring_bulk(unit_test_t * tst)
   lisp_vm_t * vm = lisp_create_vm(&lisp_vm_default_param);
   lisp_cell_t str, substr;
   lisp_make_string(vm, &str, "0123456789");
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr,
-					  LISP_AS(&str, lisp_string_t),
-					  2,
-					  4), LISP_OK);
+  ASSERT_IS_OK(tst,    lisp_make_substring(vm, 
+                                           &substr,
+                                           LISP_AS(&str, lisp_string_t),
+                                           2,
+                                           4));
 
   ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t), 
-					       "23"), 0);
+                                                       lisp_string_t), 
+                                               "23"),
+              0);
   ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "234"), 0);
+                                                       lisp_string_t),
+                                               "234"),
+              0);
   ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "34"), 0);
+                                                       lisp_string_t),
+                                               "34"),
+              0);
   ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "00"), 0);
+                                                       lisp_string_t),
+                                               "00"),
+              0);
   ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "01"), 0);
+                                                       lisp_string_t),
+                                               "01"),
+              0);
 
-  lisp_unset_object(vm, &str);
-  lisp_unset_object(vm, &substr);
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &substr));
 
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
@@ -259,31 +261,35 @@ static void test_make_substring_end(unit_test_t * tst)
   lisp_vm_t * vm = lisp_create_vm(&lisp_vm_default_param);
   lisp_cell_t str, substr;
   lisp_make_string(vm, &str, "0123456789");
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr,
-					  LISP_AS(&str, lisp_string_t),
-					  4,
-					  10), LISP_OK);
+  ASSERT_IS_OK(tst,    lisp_make_substring(vm, 
+                                           &substr,
+                                           LISP_AS(&str, lisp_string_t),
+                                           4,
+                                           10));
 
   ASSERT_EQ_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t), 
-					       "456789"), 0);
+                                                       lisp_string_t), 
+                                               "456789"),
+              0);
   ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "4567890"), 0);
+                                                       lisp_string_t),
+                                               "4567890"),
+              0);
   ASSERT_LT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "5678901"), 0);
+                                                       lisp_string_t),
+                                               "5678901"),
+              0);
   ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "4567"), 0);
+                                                       lisp_string_t),
+                                               "4567"),
+              0);
   ASSERT_GT_I(tst,    lisp_string_cmp_c_string(LISP_AS(&substr,
-						       lisp_string_t),
-					       "345678"), 0);
+                                                       lisp_string_t),
+                                               "345678"),
+              0);
 
-
-  lisp_unset_object(vm, &str);
-  lisp_unset_object(vm, &substr);
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &str));
+  ASSERT_IS_OK(tst,   lisp_unset_object(vm, &substr));
 
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
@@ -296,32 +302,31 @@ static void test_make_substring_range_errors(unit_test_t * tst)
   lisp_vm_t * vm = lisp_create_vm(&lisp_vm_default_param);
   lisp_cell_t str, substr;
   lisp_make_string(vm, &str, "0123456789");
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr,
-					  LISP_AS(&str, lisp_string_t),
-					  5,
-					  4), LISP_RANGE_ERROR);
+  ASSERT_IS_RANGE_ERROR(tst,    lisp_make_substring(vm, 
+                                                    &substr,
+                                                    LISP_AS(&str, lisp_string_t),
+                                                    5,
+                                                    4));
   /* @todo test that substr is expception */
-  lisp_unset_object(vm, &substr);
+  ASSERT_IS_OK(tst,             lisp_unset_object(vm, &substr));
 
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr,
-					  LISP_AS(&str, lisp_string_t),
-					  11,
-					  14), LISP_RANGE_ERROR);
+  ASSERT_IS_RANGE_ERROR(tst,    lisp_make_substring(vm, 
+                                                    &substr,
+                                                    LISP_AS(&str, lisp_string_t),
+                                                    11,
+                                                    14));
   /* @todo test that substr is expception */
-  lisp_unset_object(vm, &substr);
+  ASSERT_IS_OK(tst,             lisp_unset_object(vm, &substr));
 
-
-  ASSERT_EQ_I(tst,    lisp_make_substring(vm, 
-					  &substr,
-					  LISP_AS(&str, lisp_string_t),
-					  4,
-					  14), LISP_RANGE_ERROR);
+  ASSERT_IS_RANGE_ERROR(tst,    lisp_make_substring(vm, 
+                                                    &substr,
+                                                    LISP_AS(&str, lisp_string_t),
+                                                    4,
+                                                    14));
   /* @todo test that substr is expception */
-  lisp_unset_object(vm, &substr);
+  ASSERT_IS_OK(tst,             lisp_unset_object(vm, &substr));
 
-  lisp_unset_object(vm, &str);
+  ASSERT_IS_OK(tst,             lisp_unset_object(vm, &str));
   lisp_free_vm(vm);
   ASSERT_MEMCHECK(tst);
   memcheck_end();
